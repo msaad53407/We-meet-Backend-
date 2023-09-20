c8523947988119a4bcf6db154f4c1b9fe50263e6
Muhammad Saad <pubg53407@gmail.com>
2023-09-21 00:48:56 +0500

    Adding All HTTP Routes to App

--- server.js
+++ server.js
@@ -61,7 +61,7 @@ app.post("/login", cors(), async (req, res) => {
 
 app.post("/signup", cors(), async (req, res) => {
   try {
-    const { username, password } = req.body;
+    const { name, username, password } = req.body;
     const result = await client.connect((err) => {
       if (err) {
         console.error(err);
@@ -87,6 +87,7 @@ app.post("/signup", cors(), async (req, res) => {
     } else {
       const newDocument = {
         user: {
+          name: name,
           username: username,
           password: password,
         },
@@ -119,6 +120,48 @@ app.get("/login", cors(), async (req, res) => {
     const id = req.query.id;
     const result = await client.connect((err) => {
       if (err) {
+        console.log(err);
+        return;
+      }
+      app.listen(PORT, () => {
+        console.log("Listening for requests");
+      });
+    });
+    await client.db(dbName).command({ ping: 1 });
+    console.log("Connected to MongoDB Successfully");
+    const db = result.db(dbName);
+
+    const userData = db.collection("userData");
+
+    const query = { _id: new ObjectId(id) };
+    const user = await userData.findOne(query);
+    if (!user) {
+      console.log(id);
+      res.status(200).json({
+        loginCondition: false,
+        loginData: "No User found with username",
+      });
+    } else {
+      console.log(id);
+      res.status(200).json({
+        loginCondition: true,
+        loginData: user,
+      });
+    }
+  } catch (error) {
+    res.status(500).send(error);
+    console.log(error);
+  } finally {
+    client.close();
+    console.log("client closed");
+  }
+});
+
+app.post("/post", cors(), async (req, res) => {
+  try {
+    const { userId, tweetContent } = req.body;
+    const result = await client.connect((err) => {
+      if (err) {
         console.error(err);
         return false;
       }
@@ -131,28 +174,184 @@ app.get("/login", cors(), async (req, res) => {
     console.log("Connected to MongoDB Successfully");
     const db = result.db(dbName);
 
+    const PostsData = db.collection("PostsData");
     const userData = db.collection("userData");
-
-    const query = { _id: new ObjectId(id) };
-
+    const query = { _id: new ObjectId(userId) };
     const user = await userData.findOne(query);
-    if (!user) {
+    const response = await PostsData.insertOne({
+      user: {
+        userId,
+        username: user.user.username,
+        name: user.user.name,
+      },
+      tweetContent,
+      statistics: {
+        likes: {
+          likesCount: 0,
+          usersLiked: [],
+        },
+        comments: 0,
+        shares: 0,
+      },
+      postedAt: new Date().getTime(),
+    });
+
+    if (response.acknowledged) {
       res.status(200).json({
-        loginCondition: false,
-        loginData: "No User found with username",
+        status: "Posted Successfully.",
       });
     } else {
-      res.status(200).json({
-        loginCondition: true,
-        loginData: user,
+      res.status(300).json({
+        status: "Could not Post",
       });
     }
   } catch (error) {
-    res.status(500).send(error);
     console.log(error);
+    res.status(500).json({
+      status: "Internal Server Error" + error,
+    });
   } finally {
-    console.log("client closed");
     client.close();
+    console.log("Client Closed");
+  }
+});
+
+app.put("/post", cors(), async (req, res) => {
+  try {
+    const { tweetId, modifyType, userId } = req.body;
+    const result = await client.connect((err) => {
+      if (err) {
+        console.error(err);
+        return false;
+      }
+      // connection to mongo is successful, listen for requests
+      app.listen(PORT, () => {
+        console.log("listening for requests");
+      });
+    });
+    await client.db(dbName).command({ ping: 1 });
+    console.log("Connected to MongoDB Successfully");
+    const db = result.db(dbName);
+
+    const PostsData = db.collection("PostsData");
+
+    const query = { _id: new ObjectId(tweetId) };
+    switch (modifyType) {
+      case "like":
+        const incrementLike = {
+          $inc: { "statistics.likes.likesCount": 1 },
+          $push: {
+            "statistics.likes.usersLiked": {
+              userId: userId,
+            },
+          },
+        };
+        const updatedPost = await PostsData.findOneAndUpdate(
+          query,
+          incrementLike
+        );
+        res.status(200).json({
+          status: "Liked Successfully.",
+          data: updatedPost,
+        });
+        break;
+      case "unlike":
+        const decrementLike = {
+          $inc: { "statistics.likes.likesCount": -1 },
+          $pull: { "statistics.likes.usersLiked": { userId } },
+        };
+        const unlikeDocument = await PostsData.findOne(query);
+        if (unlikeDocument && unlikeDocument.statistics.likes.likesCount > 0) {
+          const post = await PostsData.updateOne(query, decrementLike);
+          res.status(200).json({
+            status: "Unliked Successfully.",
+            data: post,
+          });
+          break;
+        } else {
+          res.status(200).json({
+            status: "Cannot Decrement likes below 0",
+          });
+          break;
+        }
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      status: "Internal Server Error" + error,
+    });
+  } finally {
+    client.close();
+    console.log("Client Closed");
+  }
+});
+
+app.delete("/post-delete", cors(), async (req, res) => {
+  try {
+    const { tweetId } = req.body;
+    const result = await client.connect((err) => {
+      if (err) {
+        console.error(err);
+        return false;
+      }
+      // connection to mongo is successful, listen for requests
+      app.listen(PORT, () => {
+        console.log("listening for requests");
+      });
+    });
+    await client.db(dbName).command({ ping: 1 });
+    console.log("Connected to MongoDB Successfully");
+    const db = result.db(dbName);
+
+    const PostsData = db.collection("PostsData");
+
+    const query = { _id: new ObjectId(tweetId) };
+    console.log(await PostsData.findOne(query));
+    await PostsData.findOneAndDelete(query);
+    res.status(200).json({
+      status: "Post Deleted Successfully.",
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      status: "Internal Server Error" + error,
+    });
+  } finally {
+    client.close();
+    console.log("Client Closed");
+  }
+});
+
+app.get("/posts", cors(), async (req, res) => {
+  try {
+    const result = await client.connect((err) => {
+      if (err) {
+        console.error(err);
+        return false;
+      }
+      // connection to mongo is successful, listen for requests
+      app.listen(PORT, () => {
+        console.log("listening for requests");
+      });
+    });
+    await client.db(dbName).command({ ping: 1 });
+    console.log("Connected to MongoDB Successfully");
+    const db = result.db(dbName);
+
+    const PostsData = db.collection("PostsData");
+
+    const posts = await PostsData.find({}).limit(10).toArray();
+    res.status(200).json({
+      posts,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      status: "Internal Server Error" + error,
+    });
+  } finally {
+    client.close();
+    console.log("Client Closed");
   }
 });
 
